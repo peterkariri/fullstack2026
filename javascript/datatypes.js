cd9a90c9d67eca7f580c52ae09ef55bac6d607cc
spectre <peternjugunaw25@gmail.com>
2026-03-25 20:51:36 +0300

    objects

--- javascript/datatypes.js
+++ javascript/datatypes.js
@@ -39,3 +39,95 @@
     // to write a string that spans multiple lines we can use
     //  template literals with backticks and it will preserve the line breaks and spacing in the string.
     
+/* number>>they ae used to represent integers(ANY NUMBER WITHOUT A DECIMAL POINT)
+ and floating point(NUMBERS WITH A DECIMAL POINT) number 
+ including special numbers such as infinity and NaN(not a number)
+ 
+ we use the typeof operator to check the type of a variable or a data type */ 
+ let age=34; /* INTEGER */
+   let price=19.99; /* FLOATING POINT */
+   let infinity=Infinity; //represents a value that is greater than any other number
+   let notANumber=NaN; //represents a value that is not a number
+   console.log(typeof(infinity));
+   console.log(typeof(notANumber));
+   console.log(typeof(age));
+   console.log(typeof(price));
+   
+   NaN===NaN
+   
+   let numberOne=10;
+   let numberTwo="10";
+   console.log(numberOne==numberTwo); //true >>coersion happens here and the string "10"
+   //  is converted to the number 10 before the comparison is made
+   console.log(numberOne===numberTwo);//false>>strict equality operator checks for both 
+   // value and type and since numberOne is a number and numberTwo is a string, 
+   // they are not strictly equal.
+   
+   console.log("5"/2);
+   
+  /*  bolean>>used to check wether a condition is true or false
+   you are either 1 or 0, true or false, on or off, yes or no
+   they are often used in conditional statements and loops to control the flow of the program.
+   we use the exclamation mark to apply a negation to a statement >>negate operator 
+  */
+ let isLoggedIn=true;
+ let hasPermission=false;//restricting the user from the platform
+ console.log(!0);
+  console.log(!!0);//the first exclamation mark negates the value of 0, 
+  // which is falsy, so it becomes true. The second exclamation mark negates the value of true,
+  //  which is truthy, so it becomes false. Therefore, !!0 evaluates to false.)
+
+ 
+ console.log(typeof(isLoggedIn));
+ 
+/*  undefined>>this is used to refer to something that has been called and was not passed in a value
+
+a variable that has been declared but not assigned a value */
+let firstname;
+console.log(firstname);
+
+/* null>>represents intentional absence of a value 
+
+you want it to be a placeholder for a value that will be assigned
+ later or to indicate that a variable should not have a value.
+*/
+let y=null;
+console.log(y);
+y="peter"
+console.log(y);
+
+/* non primitive data types 
+this are the most common and important datatypes 
+in javascript and they include objects, arrays, functions, regex,and dates.
+*/
+/* objects this are a collection of key value pairs that are used to store data 
+we defie objects using a few methods the first and most commmon is 
+object literal syntax where we use the variable name the assignment operator and the curly
+ brackets to enclose the data sets where 
+the key and value are seperated with a colon just like in css property
+ and value.we seperate the key value pair with comms
+*/
+const person={
+   name:"Remmy Opondo",
+   age:34,
+   isStudent:true,
+   hobbies:["coding","reading","traveling"],
+   address:{
+      city:"Nairobi",
+      country:"Kenya",
+      postalCode:3100
+   }
+}
+/* to access elements in an object we use the dot Notification,bracket notation */ 
+/* the dot Notification */
+console.log(person.name);
+/* the bracket notation >>you will have to have some components in quotes based on their data type*/
+console.log(person["name"]);
+
+let properties=`My name is ${person.name} and i am ${person.age}
+ years old and my hobby is ${person.hobbies[0]}
+ and i live in ${person.address.city}, in ${person.address.country} 
+ and my postal code is ${person.address.postalCode}`;
+
+ console.log(properties);
+ 
