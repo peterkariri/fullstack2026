5ac5dc3e2ef5efe91bd1e59d50c4a44f78cffea7
spectrenjuguna <peternjugunaw25@gmail.com>
2026-07-13 20:51:57 +0300

    connection

--- ecommerce/server.js
+++ ecommerce/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const mysql=require('mysql2')
 const port = 3000;
 
 // Path module helps locate files and folders
@@ -9,6 +9,43 @@ const path = require('path');
 app.set("view engine" ,"ejs")
 // MIDDLEWARES
 // Middleware acts as a bridge between the request and response
+
+//once mysql2 has been importes we create.  connection pool to our local database
+//we use the createpool() or createConnection()
+/* the connction has properties in it to link to database this includ {
+    host:'localhost'
+    password:''
+    user:'root'
+    database :'name' 
+
+} */
+let pool=mysql.createPool(
+    {
+        host:'localhost',
+        user:'root',
+        password:'',
+        database:'ECOMMMERCE'
+    }
+)
+/* to test this connection we use the getConnection methods which takes in two parameters {
+    an error and a successful connection 
+    we do this by chaining the pool to getConnecton method
+} */
+
+    pool.getConnection((err,connection)=>{
+        if(err){
+              console.log("database unsuccefful");
+              
+        }
+        else{
+            console.log("database connected successfully");
+
+            connection.release()
+            
+        }
+        
+    })
+
 
 // Handles form data
 app.use(express.urlencoded({ extended: true }));
